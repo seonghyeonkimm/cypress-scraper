09fcdfc761383ab146eb1098f7818a7c7c397bce
demian <demian@kcd.co.kr>
2021-09-22 23:55:00 +0900

    Add check_jirye_reservation

---
+++ cypress/integration/check_jirye_reservation.spec.js
@@ -0,0 +1,38 @@
+const findEmptyRoom = (year, month) => {
+  cy.visit('http://www.jirye.com/Book/booklist.php', {
+    method: 'POST',
+    body: {
+      Syear: year,
+      Smonth: month,
+    },
+  });
+
+  cy.get('span[title="예약가능"]').should($elements => {
+    const data = $elements.map((i, el) => {
+      const date = el.parentNode.parentNode.querySelector('h1').textContent.trim();
+      const name = el.parentNode.textContent;
+      const matched = /^예 \d\. (.+)/.exec(name);
+      if (matched) {
+        return `(${year}년 ${month}월 ${date.padStart(2, '0')}일) ${matched[1]}`;
+      }
+  
+      return null;
+    }).filter(Boolean).get();
+    
+    return data;
+  });
+}
+
+describe('example to-do app', () => {
+  it('check 2021-10', () => {
+    findEmptyRoom(2021, 10);
+  });
+
+  it('check 2021-11', () => {
+    findEmptyRoom(2021, 11);
+  });
+
+  it('check 2021-12', () => {
+    findEmptyRoom(2021, 12);
+  });
+})
